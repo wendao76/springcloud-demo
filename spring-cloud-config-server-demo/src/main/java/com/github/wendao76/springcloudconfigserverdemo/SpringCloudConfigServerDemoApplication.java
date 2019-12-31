a45827bcbfe5a59a53942e51534825028c1ff65e
李文虎 <liwenhu@cyou-inc.com>
2019-12-31 16:30:21 +0800

    服务网关简单接入

---
+++ spring-cloud-config-server-demo/src/main/java/com/github/wendao76/springcloudconfigserverdemo/SpringCloudConfigServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.wendao76.springcloudconfigserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerDemoApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-zuul-demo/src/test/java/com/github/wendao76/springcloudzuuldemo/SpringCloudZuulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wendao76.springcloudzuuldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudZuulDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
