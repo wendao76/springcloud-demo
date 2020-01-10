5b8c883a4781281adc6fdb40600187ea933ce59b
李文虎 <liwenhu@cyou-inc.com>
2020-01-10 18:14:25 +0800

    加入sleuth链路追踪配置

--- service-consumer-demo/src/main/java/com/github/wendao76/serviceconsumerdemo/ServiceConsumerDemoApplication.java
+++ service-consumer-demo/src/main/java/com/github/wendao76/serviceconsumerdemo/ServiceConsumerDemoApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 服务消费者
+ * @author wendao76
+ */
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
--- spring-cloud-config-server-demo/src/main/java/com/github/wendao76/springcloudconfigserverdemo/SpringCloudConfigServerDemoApplication.java
+++ spring-cloud-config-server-demo/src/main/java/com/github/wendao76/springcloudconfigserverdemo/SpringCloudConfigServerDemoApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 配置中心服务
+ * @author wendao76
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class SpringCloudConfigServerDemoApplication {
