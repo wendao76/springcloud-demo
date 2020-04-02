99d816f0b3b664fe0fb9045324ae5c2018f5ef89
wendao76 <tiger900721@163.com>
2020-04-02 18:02:33 +0800

    test

--- mvc-service-consumer/src/main/java/com/github/wendao76/springcloud/serviceconsumerdemo/MvcServiceConsumerServer.java
+++ mvc-service-consumer/src/main/java/com/github/wendao76/springcloud/serviceconsumerdemo/MvcServiceConsumerServer.java
@@ -2,7 +2,7 @@ package com.github.wendao76.springcloud.serviceconsumerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class MvcServiceConsumerServer {
 
     public static void main(String[] args) {
