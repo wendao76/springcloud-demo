221947c49cd7b380fbba9a0de050c1c033abcf9b
wendao76 <tiger900721@163.com>
2020-04-07 14:35:35 +0800

    test

---
+++ assembly-start/src/main/java/com/github/wendao76/springcloud/assembly/TestMain.java
@@ -0,0 +1,15 @@
+package com.github.wendao76.springcloud.assembly;
+
+/**
+ * @Description 类描述信息
+ * @ClassName TestMain
+ * @Author wendao76
+ * @Date 2020-4-3 9:18
+ * @Version 1.0
+ */
+public class TestMain {
+	public static void main(String[] args) {
+		//
+		System.out.println(System.currentTimeMillis());
+	}
+}
--- mvc-service-provider/src/main/java/com/github/wendao76/springcloud/serviceproviderdemo/controller/IndexController.java
+++ mvc-service-provider/src/main/java/com/github/wendao76/springcloud/serviceproviderdemo/controller/IndexController.java
@@ -1,18 +1,21 @@
 package com.github.wendao76.springcloud.serviceproviderdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/v1")
 public class IndexController {
+    @Value("${spring.application.name}")
+    String appName;
+
     @PostMapping("/hello")
-    public String hello(@RequestBody Map<String, Object> params) {
-        System.out.println(params.get("a"));
-        return "Hello wendao76";
+    public String hello(HttpServletRequest request) {
+        String accessToken = request.getHeader("access_token");
+        return "Hello " + appName;
     }
 }
