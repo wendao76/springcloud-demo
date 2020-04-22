435e2b5ad790ff79aab1dc82c4e89254395fa016
wendao76 <tiger900721@163.com>
2020-04-22 10:53:38 +0800

    配置信息调整

--- mvc-service-provider/src/main/java/com/github/wendao76/springcloud/serviceproviderdemo/controller/IndexController.java
+++ mvc-service-provider/src/main/java/com/github/wendao76/springcloud/serviceproviderdemo/controller/IndexController.java
@@ -1,21 +1,20 @@
 package com.github.wendao76.springcloud.serviceproviderdemo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/")
 public class IndexController {
     @Value("${spring.application.name}")
     String appName;
 
-    @PostMapping("/hello")
+    @GetMapping("/hello")
     public String hello(HttpServletRequest request) {
-        String accessToken = request.getHeader("access_token");
-        return "Hello " + appName;
+        return "Hello MVC-SERVICE-PRODUCER";
     }
 }
