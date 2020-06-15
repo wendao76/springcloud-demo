696e27c50d6ac061dd41e89c91d36c4df84bcfdf
wendao76 <tiger900721@163.com>
2020-06-15 14:23:48 +0800

    服务状态测试

---
+++ eureka-server/src/main/java/com/github/wendao76/springcloud/springcloud/eurekaserver/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.github.wendao76.springcloud.springcloud.eurekaserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wendao76
+ * @version 1.0
+ * @description 类描述信息
+ * @className IndexController
+ * @date 2020-6-11 10:35
+ */
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+	@GetMapping("/{id}")
+	public String index(@PathVariable String id) {
+		return "Eureka Server is Running";
+	}
+}
