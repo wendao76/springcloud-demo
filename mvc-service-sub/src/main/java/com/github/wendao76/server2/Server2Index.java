41f204e078b4088ac161f0e84a2bdf199c234815
李文虎 <tiger900721@163.com>
2021-04-09 18:48:44 +0800

    加入sleuth例子

---
+++ mvc-service-sub/src/main/java/com/github/wendao76/server2/Server2Index.java
@@ -0,0 +1,9 @@
+package com.github.wendao76.server2;/**
+ * @description 类描述信息
+ * @className Server2Index
+ * @author wendao76
+ * @date 2020-9-28 16:58
+ * @version 1.0
+ */
+public class Server2Index {
+}
---
+++ springcloud-sleuth/src/main/java/com/github/wendao76/springcloud/springcloudsleuthdemo/CustomizeConfig.java
@@ -0,0 +1,11 @@
+package com.github.wendao76.springcloud.springcloudsleuthdemo;
+
+/**
+ * @author wendao76
+ * @version 1.0
+ * @description 类描述信息
+ * @className CustomizeConfig
+ * @date 2021-4-9 16:24
+ */
+public class CustomizeConfig {
+}
